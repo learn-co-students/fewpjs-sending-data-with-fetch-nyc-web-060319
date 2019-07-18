852c4af1e31d5019176d8d06cee122276a64e02d
github username <github email address>
2019-07-18 16:30:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email
+    }
+    
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+    
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.body.innerHTML = document.body.innerHTML + object.id.toString()
+            return object.id
+        })
+        .catch(function(message) {
+            message = "Unauthorized Access"
+            document.body.innerHTML = document.body.innerHTML + message
+        })
+}
+
